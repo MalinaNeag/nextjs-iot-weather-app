c03235e576d32a41ed46ee8f7b92750011bb48cb
MalinaNeag <malina.neag@student.upt.ro>
2024-04-28 16:52:53 +0300

    Added logo

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,27 +1,22 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
 import React from "react";
-import { github } from "../utils/Icons";
-import ThemeDropdown from "./ThemeDropdown/ThemeDropdown";
-import SearchDialog from "./SearchDialog/SearchDialog";
 import { useGlobalContext } from "../context/globalContext";
+import ThemeDropdown from "./ThemeDropdown/ThemeDropdown";
 
 function Navbar() {
-  const router = useRouter();
-  const { state } = useGlobalContext();
+    const { state } = useGlobalContext();
 
-  return (
-    <div className="w-full py-4 flex items-center justify-between">
-      <div className="left"></div>
-      <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
-        <SearchDialog />
-        <div className="btn-group flex items-center gap-2">
-          <ThemeDropdown />
+    return (
+        <div className="w-full py-4 flex items-center justify-between">
+            <div className="left">
+                <span className="text-lg font-bold">WeatherTM</span>
+            </div>
+            <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
+                <div className="btn-group flex items-center gap-2">
+                    <ThemeDropdown />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Navbar;
