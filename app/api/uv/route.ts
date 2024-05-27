26f8cdb4b3fe6eaff17d1a0da52b67d9db5e3f30
MalinaNeag <malina.neag@student.upt.ro>
2024-05-27 21:13:16 +0300

    Forcing dynamic rendering

--- app/api/uv/route.ts
+++ app/api/uv/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic'; // Force dynamic rendering
+
 export async function GET(req: NextRequest) {
   try {
     const url = new URL(req.url);
